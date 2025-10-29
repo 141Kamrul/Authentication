d8875c716a5c0ac3ecb87329b08bfb33278938ef
Md. Kamrul Hasan <bsse1418@iit.du.ac.bd>
2025-10-29 12:13:47 +0600

    authentication 21 ?

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,41 +2,82 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        <!-- First Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <x-form-label for="first_name" :value="__('First Name')" required />
+            <x-form-input 
+                id="first_name" 
+                class="block mt-1 w-full" 
+                type="text" 
+                name="first_name" 
+                :value="old('first_name')" 
+                required 
+                autofocus 
+                autocomplete="given-name"
+                :error="$errors->has('first_name')"
+            />
+            <x-form-error :messages="$errors->get('first_name')" class="mt-2" />
+        </div>
+
+        <!-- Last Name -->
+        <div class="mt-4">
+            <x-form-label for="last_name" :value="__('Last Name')" required />
+            <x-form-input 
+                id="last_name" 
+                class="block mt-1 w-full" 
+                type="text" 
+                name="last_name" 
+                :value="old('last_name')" 
+                required 
+                autocomplete="family-name"
+                :error="$errors->has('last_name')"
+            />
+            <x-form-error :messages="$errors->get('last_name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-form-label for="email" :value="__('Email')" required />
+            <x-form-input 
+                id="email" 
+                class="block mt-1 w-full" 
+                type="email" 
+                name="email" 
+                :value="old('email')" 
+                required 
+                autocomplete="email"
+                :error="$errors->has('email')"
+            />
+            <x-form-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-form-label for="password" :value="__('Password')" required />
+            <x-form-input 
+                id="password" 
+                class="block mt-1 w-full"
+                type="password"
+                name="password"
+                required 
+                autocomplete="new-password"
+                :error="$errors->has('password')"
+            />
+            <x-form-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-form-label for="password_confirmation" :value="__('Confirm Password')" required />
+            <x-form-input 
+                id="password_confirmation" 
+                class="block mt-1 w-full"
+                type="password"
+                name="password_confirmation" 
+                required 
+                autocomplete="new-password"
+            />
+            <x-form-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
